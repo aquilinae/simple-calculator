d88fa6a55ea70c399470fd5f64e9f7c98dcbabc7
Nikolai <ornichola@users.noreply.github.com>
2017-09-20 17:56:24 +0300

    Doing practice for Module 1.2

---
+++ [stepik]/java187/Alive.java
@@ -0,0 +1,19 @@
+// [STEPIK]
+// Java. Базовый курс https://stepik.org/course/187
+// 01_02_14 Первый контакт — Hello World
+
+/*
+Напишите программу, выводящую в консоль It's alive! It's alive!
+
+Класс должен называться "Main", это ограничение проверяющей системы.
+
+Sample Input:
+Sample Output:
+It's alive! It's alive!
+*/
+
+public class Alive {
+  public static void main(String[] args) {
+    System.out.println("It's alive! It's alive!");
+  }
+}
