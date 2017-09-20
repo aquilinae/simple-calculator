4be0316f411e7bb0d025e8d813697de6faae9a87
Nikolai <ornichola@users.noreply.github.com>
2017-09-20 18:57:44 +0300

    Setting up IntelliJ IDEA

---
+++ [stepik]/java187/HelloWorld/src/YetAnotherHelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * A program that prints <code>Hello world!</code>
+ *
+ * @author ornichola
+ * @version 1.0
+ */
+public class YetAnotherHelloWorld {
+    /**
+     * Program entry point:
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] cmdLineArgs) {
+        System.out.printf("hello world!");
+    // single line comment
+
+    /*
+    multi-line comment
+    */
+    }
+}
